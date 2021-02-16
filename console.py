3343f0fe42f24c701de479d9f0706d6515c198b1
Battsetseg20 <tsetseg.yo@gmail.com>
2021-02-16 23:17:11 +0000

    update console

--- console.py
+++ console.py
@@ -11,7 +11,7 @@ from models.base_model import BaseModel
 from models.user import User
 
 
-classes = {'BaseModel': BaseModel}
+classes = {'BaseModel': BaseModel, "User": User}
 
 class HBNBCommand(cmd.Cmd):
     """
