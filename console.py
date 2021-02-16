a90e2f1740d5405ed1a642e4aa488b543d2ba1e2
Battsetseg20 <tsetseg.yo@gmail.com>
2021-02-16 19:26:56 +0000

    add task 7

--- console.py
+++ console.py
@@ -9,14 +9,16 @@ import models
 import json
 import shlex
 
+
 classes = {'BaseModel': BaseModel}
 
 
 class HBNBCommand(cmd.Cmd):
-    """Initializing all attributes and methods:
+    """
+    Initializing all attributes and methods:
     prompt: command prompt for Airbnb console
     """
-    prompt = '(hbnb)'
+    prompt = '(hbnb) '
 
     def emptyline(self):
         """ENTER shouldn't execute anything, thus does nothing"""
@@ -31,18 +33,103 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     def do_create(self, *line):
-        """Creates new instance of class"""
+        """
+        Creates new instance of class
+        Syntax:create <class>
+        """
         parsed_line = shlex.split(*line)
         if not parsed_line:
             print("** class name missing **")
-            return False
-        if parsed_line[0] in classes:
+        elif parsed_line[0] in classes:
             new_instance = classes[parsed_line[0]]()
+            print(new_instance.id)
+            new_instance.save()
         else:
             print("** class doesn't exist **")
-            return False
-        print(new_instance.id)
-        new_instance.save()
+
+    def do_show(self, *line):
+        """
+        Prints str representation of an instance
+        Syntax: show <class name> <id>
+        """
+        parsed_line = shlex.split(*line)
+        if not parsed_line:
+            print("** class name missing **")
+        elif parsed_line[0] not in classes:
+            print("** class doesn't exist **")
+        elif len(parsed_line) == 1:
+            print("** instance id missing **")
+        else:
+            name_id = parsed_line[0] + "." + parsed_line[1]
+            if name_id in models.storage.all():
+                print(models.storage.all()[name_id])
+            else:
+                print("** no instance found **")
+
+    def do_destroy(self, *line):
+        """
+        Deletes and instance:
+        Syntax: destroy <class> <id>
+        Saves the change into the JSON file
+        """
+        parsed_line = shlex.split(*line)
+        if not parsed_line:
+            print("**class name missing **")
+        elif parsed_line[0] not in classes:
+            print("** class doesn't exist **")
+        elif len(parsed_line) == 1:
+            print("** instance id missing **")
+        else:
+            name_id = parsed_line[0] + "." + parsed_line[1]
+            if name_id in models.storage.all():
+                del models.storage.all()[name_id]
+                models.storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, *line):
+        """
+        Prints all str representation of all instances based
+        on OR not on the class name.
+        Syntax: all <class> OR all
+        """
+        parsed_line = shlex.split(*line)
+        if not parsed_line:
+            for value in models.storage.all().values():
+                print(value)
+        elif parsed_line[0] in classes:
+            for key, value in models.storage.all().items():
+                if parsed_line[0] in key:
+                    print(value)
+        else:
+            print("** class doesn't exist **")
+
+    def do_update(self, *line):
+        """
+        Updates an instance based on the class name and id by
+        adding or updating attribute
+        Syntax: update <class name> <id> <attribute name> "<attribute value>"
+        Saves the change into JSON file
+        """
+        parsed_line = shlex.split(*line)
+        if len(parsed_line) == 0:
+            print("** class name missing **")
+        elif len(parsed_line) == 1:
+            print("** instance id missing **")
+        elif len(parsed_line) == 2:
+            print("** attribute name missing **")
+        elif len(parsed_line) == 3:
+            print("** value missing **")
+        elif parsed_line[0] not in classes:
+            print("** class doesn't exist **")
+        else:
+            name_id = parsed_line[0] + "." + parsed_line[1]
+            obj = models.storage.all()[name_id]
+            name_attrib = parsed_line[2]
+            value_attrib = parsed_line[3]
+            setattr(obj, name_attrib, value_attrib)
+            models.storage.save()
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
