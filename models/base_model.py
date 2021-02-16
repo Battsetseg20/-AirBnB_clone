62b86fea66a2502e5ea55550136001b26055f1a5
Battsetseg20 <tsetseg.yo@gmail.com>
2021-02-16 10:32:45 +0000

    PEP8 validated

--- models/base_model.py
+++ models/base_model.py
@@ -32,24 +32,23 @@ class BaseModel():
                         self.__dict__[key] = value
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now(tz=None)
-            self.updated_at = datetime.now(tz=None)
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
-
 
     def __str__(self):
         """
         Returns string representation of the BaseModel
         format: [<class name>] (<self.id>) <self.__dict__>
         """
-        return "[{}] ({}) {}".format(
+        return "[{:s}] ({:s}) {}".format(
             self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         """
         Updates  updated_at with current datetime
         """
-        self.updated_at = datetime.now(tz=None)
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
