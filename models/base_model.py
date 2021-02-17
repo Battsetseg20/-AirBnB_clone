677624a5e1e13ad86eeee29309b65fe042e3650b
Battsetseg20 <tsetseg.yo@gmail.com>
2021-02-17 08:56:09 +0000

    testing

--- models/base_model.py
+++ models/base_model.py
@@ -21,7 +21,8 @@ class BaseModel():
 
         Otherwise: id and created_at assigned when instance is created
         """
-
+        self.id = str(uuid.uuid4())
+        self.created_at = self.updated_at = datetime.now()
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
@@ -31,9 +32,6 @@ class BaseModel():
                     if key != '__class__':
                         self.__dict__[key] = value
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
             models.storage.new(self)
 
     def __str__(self):
