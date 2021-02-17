3d826a155c96a59968e61e5bf5cf436697ad0958
Battsetseg20 <tsetseg.yo@gmail.com>
2021-02-17 10:47:11 +0000

    update test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -13,7 +13,7 @@ class TestFileStorage(unittest.TestCase):
         self.storage = FileStorage()
         self.base1 = BaseModel()
 
-    def test_intance(self):
+    def test_instance(self):
         """Checks for the storage instance"""
         self.assertIsInstance(self.storage, FileStorage)
 
@@ -21,3 +21,10 @@ class TestFileStorage(unittest.TestCase):
         """Checks for attributes"""
         self.assertEqual(str, type(FileStorage._FileStorage__file_path))
         self.assertEqual(dict, type(FileStorage._FileStorage__objects))
+
+    def test_all(self):
+        """Checks if all method returns dict"""
+        test_dict = models.storage.all()
+        self.assertEqual(dict, type(test_dict))
+        self.assertIs(test_dict, FileStorage._FileStorage__objects)
+        
