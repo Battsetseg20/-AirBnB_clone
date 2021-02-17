0a0149cbd7add5d8dcde7e36e9f6a28aab8fcc54
Battsetseg20 <tsetseg.yo@gmail.com>
2021-02-17 09:33:45 +0000

    add test storage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Unittests for the file_storage module"""
+import unittest
+from models.engine.file_storate import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+    """Tests for the functionality"""
+    def setUp(self):
+        """Method setus up for testing"""
+        pass
