42c6665877f85a0c73dab4ffd0294b2f52906693
Battsetseg20 <tsetseg.yo@gmail.com>
2021-02-17 09:21:29 +0000

    back to older version

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -38,7 +38,7 @@ class FileStorage():
     def save(self):
         """Serializes __objects to JSON file (path:__file_path)"""
         new_dict = {}
-        for key, value in self.__objects.items():
+        for key, value _in self.__objects.items():
             new_dict[key] = value.to_dict()
         with open(self.__file_path, 'w') as f:
             json.dump(new_dict, f)
@@ -47,10 +47,10 @@ class FileStorage():
         """Deserializes the JSON file to __objects
         if __file_path doesn't exist, nothing is done
         """
-        try:
+        if exists(self.__file_path) is False:
+            return
+        else:
             with open(self.__file_path, 'r') as f:
                 for key, value in (json.load(f)).items():
                     value = eval(value["__class__"])(**value)
                     self.__objects[key] = value
-        except FileNotFoundError:
-            pass
